bb76ce10ca0a313302ad35d5eb820ce93bad054c
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2021-02-16 16:56:36 +0100

    spns #23875 OpenTurns

---
+++ products/env_scripts/openturns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import os.path
+
+def set_env(env, prereq_dir, version):
+    env.set('OPENTURN_ROOT_DIR', prereq_dir)
+    env.prepend('PATH', os.path.join(prereq_dir,'bin'))
+    pyver = 'python' + env.get('PYTHON_VERSION')
+    env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib', pyver, 'site-packages'))
+
+def set_nativ_env(env):
+    pass
