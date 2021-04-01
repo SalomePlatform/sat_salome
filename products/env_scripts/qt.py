1c39dcd3637a934036964d95f63f1174e4da30e3
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2021-04-01 11:53:49 +0200

    SALOME 9.7.0 starting from a consistent master

--- products/env_scripts/qt.py
+++ products/env_scripts/qt.py
@@ -23,14 +23,21 @@ def set_env(env, prereq_dir, version):
         env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
 
 def set_nativ_env(env):
+
+    # Adapt Qt libraries dir according to the current distribution (generic -> Ubuntu)
+    if any(distribution in platform.platform() for distribution in ["debian", "generic"]) :
+        qt_lib_dir='/usr/lib/x86_64-linux-gnu'
+    else :
+        qt_lib_dir='/usr/lib64'
+
     env.set('QT5_ROOT_DIR', '/usr')
-    env.set('Qt5Core_DIR', '/usr/lib64/cmake/Qt5Core')
-    env.set('Qt5Gui_DIR', '/usr/lib64/cmake/Qt5Gui')
-    env.set('Qt5Widgets_DIR', '/usr/lib64/cmake/Qt5Widgets')
-    env.set('Qt5Network_DIR', '/usr/lib64/cmake/Qt5Network')
-    env.set('Qt5Xml_DIR', '/usr/lib64/cmake/Qt5Xml')
-    env.set('Qt5OpenGL_DIR', '/usr/lib64/cmake/Qt5OpenGL')
-    env.set('Qt5PrintSupport_DIR', '/usr/lib64/cmake/Qt5PrintSupport')
-    env.set('Qt5Help_DIR', '/usr/lib64/cmake/Qt5Help')
-    env.set('Qt5Test_DIR', '/usr/lib64/cmake/Qt5Test')
-    env.set('Qt5X11Extras_DIR', '/usr/lib64/cmake/Qt5X11Extras')
+    env.set('Qt5Core_DIR', os.path.join(qt_lib_dir, 'cmake/Qt5Core'))
+    env.set('Qt5Gui_DIR', os.path.join(qt_lib_dir, 'cmake/Qt5Gui'))
+    env.set('Qt5Widgets_DIR', os.path.join(qt_lib_dir, 'cmake/Qt5Widgets'))
+    env.set('Qt5Network_DIR', os.path.join(qt_lib_dir, 'cmake/Qt5Network'))
+    env.set('Qt5Xml_DIR', os.path.join(qt_lib_dir, 'cmake/Qt5Xml'))
+    env.set('Qt5OpenGL_DIR', os.path.join(qt_lib_dir, 'cmake/Qt5OpenGL'))
+    env.set('Qt5PrintSupport_DIR', os.path.join(qt_lib_dir, 'cmake/Qt5PrintSupport'))
+    env.set('Qt5Help_DIR', os.path.join(qt_lib_dir, 'cmake/Qt5Help'))
+    env.set('Qt5Test_DIR', os.path.join(qt_lib_dir, 'cmake/Qt5Test'))
+    env.set('Qt5X11Extras_DIR', os.path.join(qt_lib_dir, 'cmake/Qt5X11Extras'))
