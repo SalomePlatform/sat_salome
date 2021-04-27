9efb10c17c25c2fc3c6855baa805c6a0590d7f89
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2021-04-27 12:47:19 +0200

    WINDOWS - ISPC add missing quite /Y flag to xcopy

--- products/compil_scripts/ispc-1.15.0.bat
+++ products/compil_scripts/ispc-1.15.0.bat
@@ -9,7 +9,7 @@ if NOT exist "%PRODUCT_INSTALL%\bin" mkdir %PRODUCT_INSTALL%\bin
 
 SET MSBUILDDISABLENODEREUSE=1
 cd %SOURCE_DIR%\bin
-xcopy * %PRODUCT_INSTALL%\bin /E /I /Q
+xcopy * %PRODUCT_INSTALL%\bin /E /I /Q /Y
 if NOT %ERRORLEVEL% == 0 (
     echo ERROR on xcopy
     exit 1
