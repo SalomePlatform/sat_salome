f91a1bad1e637c91b18772a6dd5f9aa23b98b6b2
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2021-07-08 18:35:41 +0200

    spns #18828: PMML post-build environment - Linux case

--- products/env_scripts/pmml.py
+++ products/env_scripts/pmml.py
@@ -7,7 +7,13 @@ def set_env(env, prereq_dir, version):
     env.set("PMML_ROOT_DIR",prereq_dir)
     env.set('PMML_INCLUDE_DIR',os.path.join(prereq_dir,'include'))
     env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+    if platform.system() == "Windows" :
+      pass
+    else:
+      env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+      pyver = 'python' + env.get('PYTHON_VERSION')
+      env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib', pyver, 'site-packages'))
+      env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'bin'))
 
-    	
 def set_nativ_env(env):
     pass
