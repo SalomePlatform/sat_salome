e50ddfc02fc352f50fed76409d74e3e02d8faca6
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2021-08-30 14:09:02 +0200

    spns #24377 : compilation cminpack apply patch for cblas

--- products/env_scripts/cminpack.py
+++ products/env_scripts/cminpack.py
@@ -2,11 +2,9 @@
 import os.path
 
 def set_env(env, prereq_dir, version):
-    env.set('OPENTURNS_ROOT_DIR', prereq_dir)
+    env.set('CMINPACK_ROOT_DIR', prereq_dir)
     env.prepend('PATH', os.path.join(prereq_dir,'bin'))
     env.prepend('LD_LIBRARY_PATH',os.path.join(prereq_dir, 'lib'))
-    pyver = 'python' + env.get('PYTHON_VERSION')
-    env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib', pyver, 'site-packages'))
 
 def set_nativ_env(env):
     pass
