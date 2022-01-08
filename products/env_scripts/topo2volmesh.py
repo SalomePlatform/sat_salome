0df090158caad9222b860e3bf769fedf71ce1606
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2022-01-08 21:35:24 +0100

    spns #19079: add topo2volmesher -  ongoing task: add it as a PARAVISADDONS plugin

---
+++ products/env_scripts/topo2volmesh.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import os
+import platform
+
+def set_env(env, prereq_dir, version):
+    env.set("TOPO2VOLMESH_ROOT_DIR",prereq_dir)
+    if not platform.system() == "Windows" :
+        env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+
+def set_nativ_env(env):
+    pass
