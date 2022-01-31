ad3dc69d08b72b915e5a3ebba94ab8c9e88a33d7
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2022-01-31 17:21:20 +0100

    spns #24419

--- products/env_scripts/openmpi.py
+++ products/env_scripts/openmpi.py
@@ -8,6 +8,7 @@ def set_env(env, prereq_dir, version):
     env.set('OPAL_PREFIX', prereq_dir) # be able to move openmpi install (packages)
     env.set('MPI_ROOT_DIR', prereq_dir)  # update for cmake  
     env.set('MPI_ROOT', prereq_dir)
+    env.set('MPI_C_FOUND', os.path.join(prereq_dir,'lib','libmpi.so'))
     root = env.get('OPENMPIDIR')
     
     env.prepend('PATH', os.path.join(root, 'bin'))
@@ -16,8 +17,18 @@ def set_env(env, prereq_dir, version):
     env.prepend('C_INCLUDE_PATH', os.path.join(root, 'include'))  # needed for parallel h5py
 
 def set_nativ_env(env):
-    env.set('MPI_ROOT_DIR', "/usr")  # update for cmake
-    env.set('OPENMPIDIR', "/usr")
-    env.set('MPI_ROOT', "/usr")
-
-
+    prereq_dir='/usr'
+    try:
+        import distro
+        if any(distribution in distro.name().lower() for distribution in ["centos", "fedora"]) :
+            prereq_dir='/usr/lib64/openmpi'
+    except:
+        import platform
+        if any(distribution in platform.linux_distribution()[0].lower() for distribution in ["centos", "fedora"]) :
+            prereq_dir='/usr/lib64/openmpi'
+    env.set('MPI_ROOT_DIR', prereq_dir)
+    env.set('OPENMPIDIR', prereq_dir)
+    env.set('MPI_ROOT', prereq_dir)
+    env.set('MPI_C_FOUND', os.path.join(prereq_dir,'lib','libmpi.so'))
+    env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir,'lib'))
