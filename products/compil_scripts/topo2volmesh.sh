2f619e1185567614c8ec8458c91f2143cb110f80
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2022-02-07 11:59:31 +0100

    erreur dans la logique pour vérifier que c'est une vm

--- products/compil_scripts/topo2volmesh.sh
+++ products/compil_scripts/topo2volmesh.sh
@@ -55,7 +55,7 @@ fi
 echo
 echo "*** Check if node is a virtual machine"
 ISVM=$(hostnamectl status|grep -i chassis:|grep vm)
-if [ $? -ne 0 ]; then
+if [ ! -z "$ISVM" ]; then
     echo "*** oversubscribe..."
     sed -i 's/mpirun -np \$(NP)/mpirun -np \$(NP) --oversubscribe/g' src/*/Makefile.am
 else
