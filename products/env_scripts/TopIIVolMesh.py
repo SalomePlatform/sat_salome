df0334b7bd63d9c33045827f9ed1ec1d9fed6c10
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2022-02-14 09:09:09 +0100

    spns #19079: rename top-ii-vol SALOME interface

--- products/env_scripts/TopIIVolMesh.py
+++ products/env_scripts/TopIIVolMesh.py
@@ -5,7 +5,7 @@ import os
 import platform
 
 def set_env(env, prereq_dir, version):
-    env.set("TOPO2VOLMESH_ROOT_DIR",prereq_dir)
+    env.set("TOPIIVOLMESH_ROOT_DIR",prereq_dir)
     if not platform.system() == "Windows" :
         env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
 
