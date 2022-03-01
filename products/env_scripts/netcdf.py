8804cb17984fedbfb34b2244818b27515ad37b64
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2022-03-01 08:48:51 +0100

    spns #26828: convergence EDF: fix typo

--- products/env_scripts/netcdf.py
+++ products/env_scripts/netcdf.py
@@ -2,8 +2,8 @@
 import os.path
 
 def set_env(env, prereq_dir, version):
-    env.set('NETCFD_ROOT_DIR', prereq_dir)
-    env.set('NETCFD_VERSION',version)
+    env.set('NETCDF_ROOT_DIR', prereq_dir)
+    env.set('NETCDF_VERSION',version)
     env.prepend('PATH', os.path.join(prereq_dir,'bin'))
     env.prepend('LD_LIBRARY_PATH',os.path.join(prereq_dir, 'lib'))
 
