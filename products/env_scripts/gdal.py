4f755bdfab938bcd4bd0b34e681921363fb8c4c2
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2022-03-01 11:22:24 +0100

    spns #26828: convergence EDF: gdal environment

--- products/env_scripts/gdal.py
+++ products/env_scripts/gdal.py
@@ -2,11 +2,17 @@
 import os.path
 
 def set_env(env, prereq_dir, version):
+    pyver = 'python' + env.get('PYTHON_VERSION')
+
     env.set('GDAL_ROOT_DIR', prereq_dir)
     env.set('GDALHOME', prereq_dir)
     env.set('GDAL_VERSION',version)
+    env.set('GDAL_DATA', os.path.join(prereq_dir, 'share', 'gdal'))
+    env.set('GDAL_ROOT', os.path.join(prereq_dir, 'root'))
+
     env.prepend('PATH', os.path.join(prereq_dir,'bin'))
     env.prepend('LD_LIBRARY_PATH',os.path.join(prereq_dir, 'lib'))
+    env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib', pyver, 'site-packages'))
 
 def set_nativ_env(env):
     pass
