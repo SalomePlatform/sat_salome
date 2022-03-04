59546ac80d9271d3527720e3003bc9d95990cccb
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2022-03-04 16:38:45 +0100

    Switch on GDAL support ParaView...

--- products/env_scripts/CAS.py
+++ products/env_scripts/CAS.py
@@ -7,7 +7,6 @@ import platform
 def set_env(env, prereq_dir, version):
     env.set('CASROOT', prereq_dir)
     env.set('CAS_ROOT_DIR', prereq_dir)
-    # [CMake GUI] Nouveau nom pour CASROOT = CAS_ROOT_DIR 22/03/2013
     env.set('OPENCASCADE_ROOT_DIR', prereq_dir)
     env.prepend('PATH', prereq_dir)
     
@@ -16,7 +15,6 @@ def set_env(env, prereq_dir, version):
     else :
         env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
         env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
-   
 
 def set_nativ_env(env):
     pass
