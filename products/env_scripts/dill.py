d345488fe4c5adafb3155773ecb283d74b1b14ff
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2022-04-01 16:11:12 +0200

    OCCT V7_5_3p2 and Openturns 1.18 Windows (note that one needs DILL 0.3.4)

---
+++ products/env_scripts/dill.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import os.path
+
+def set_env(env, prereq_dir, version):
+    env.set('DILL_ROOT_DIR', prereq_dir)
+    env.set('OT_VERSION',version)
+    pyver = 'python' + env.get('PYTHON_VERSION')
+    env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib', pyver, 'site-packages'))
+def set_nativ_env(env):
+    pass
