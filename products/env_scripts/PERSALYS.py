1d935b9c773c7d495ca358ea66d76924335b0c4e
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2022-04-12 16:49:10 +0200

    spns #29667: fix PV_PLUGIN_PATH fedora 34 crash

--- products/env_scripts/PERSALYS.py
+++ products/env_scripts/PERSALYS.py
@@ -2,12 +2,13 @@
 import os.path
 
 def set_env(env, prereq_dir, version):
+    pvversion='paraview-' + env.get('PARAVIEW_VERSION')
     env.set('PERSALYS_ROOT_DIR', prereq_dir)
     env.set('PERSALYS_VERSION',version)
     env.set('OTGUI_DIR', prereq_dir)
     env.prepend('PATH', os.path.join(prereq_dir,'bin'))
     env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
-    env.prepend('PV_PLUGIN_PATH', os.path.join(prereq_dir, 'lib'))
+    env.prepend('PV_PLUGIN_PATH', os.path.join(prereq_dir, 'lib', pvversion, 'plugins'))
     pyver = 'python' + env.get('PYTHON_VERSION')
     env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib', pyver, 'site-packages'))
     env.set('OTGUI_HTML_DOCUMENTATION_PATH',os.path.join(prereq_dir,'share','otgui','doc','html'))
