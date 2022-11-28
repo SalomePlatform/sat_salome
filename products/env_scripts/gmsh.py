2892ee7367b2abb6f47bc7dd8c62f8cb7fce8e18
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2022-11-28 14:28:24 +0100

    spns #32377: gmsh python api linux windows

--- products/env_scripts/gmsh.py
+++ products/env_scripts/gmsh.py
@@ -9,9 +9,10 @@ def set_env(env, prereq_dir, version):
   env.set('GMSH_ROOT_DIR', prereq_dir)
   if platform.system() == "Windows" :
     env.append('PATH',os.path.join(prereq_dir,'bin'))
-    env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib'))
+    env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'bin'))
   else:
     env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
+    env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib'))
 
 def set_nativ_env(env):
     env.set('GMSH_ROOT_DIR', '/usr')
