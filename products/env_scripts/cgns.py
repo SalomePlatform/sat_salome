9a45c6aa0e794aacdc67ca7a0f1aa1517a8dafb7
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2022-12-06 17:19:01 +0100

    spns #32377: detect embedded cgns for GMSH build if cgns is embedded otherwise will collide with cgns system library if any

--- products/env_scripts/cgns.py
+++ products/env_scripts/cgns.py
@@ -4,6 +4,7 @@
 import os.path, platform
 
 def set_env(env, prereq_dir, version):
+  env.set('CGNS_ROOT', prereq_dir)
   env.set('CGNS_ROOT_DIR', prereq_dir)
   env.set('CGNS_INCLUDE_DIR',os.path.join(prereq_dir,'include'))
   env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
