799c40bf13b0cf26766bd67884f23762674a18b3
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2022-12-15 16:04:37 +0100

    [EDF] expand PYTHONPATH for SliceRemesh filter -  A.Geay -  email exchange on 2022/12/15 14:58:14

--- products/env_scripts/PARAVISADDONS.py
+++ products/env_scripts/PARAVISADDONS.py
@@ -4,6 +4,7 @@ import os.path
 def set_env(env, prereq_dir, version):
     env.set('PARAVISADDONS', prereq_dir)
     env.append('PV_PLUGIN_PATH', os.path.join(prereq_dir, 'lib','paraview'))
+    env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'bin'))
 
 def set_nativ_env(env):
     pass
