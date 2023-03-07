76a75e2ba94401860a94baf9ab9d1a9f821a3def
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2023-03-07 16:03:20 +0100

    openmpi: compilation script update to be compliant with openmpi-4.x

--- products/compil_scripts/openmpi.sh
+++ products/compil_scripts/openmpi.sh
@@ -4,36 +4,46 @@ echo "##########################################################################
 echo "OpenMPI" $VERSION
 echo "##########################################################################"
 
+rm -rf $BUILD_DIR
+mkdir $BUILD_DIR
+cd $BUILD_DIR
 
+cp -r $SOURCE_DIR/* .
 
-# Attention la variable $PYTHOSTARTUP pose problem -> unset.
-# Si erreur au make install -> environement "pollué"
+if [ -f autogen.pl ]; then
+    echo
+    echo "*** autoreconf -i"
+    chmod +x autogen.pl
+    ./autogen.pl
+    if [ $? -ne 0 ]
+    then
+        echo "ERROR on autogen.pl"
+        exit 1
+    fi
+fi
 
 echo
 echo "*** configure"
-$SOURCE_DIR/configure --prefix=$PRODUCT_INSTALL
-if [ $? -ne 0 ]
-then
+$BUILD_DIR/configure --prefix=$PRODUCT_INSTALL
+if [ $? -ne 0 ]; then
     echo "ERROR on configure"
-    exit 1
+    exit 2
 fi
 
 echo
 echo "*** make" $MAKE_OPTIONS
 make $MAKE_OPTIONS
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
     echo "ERROR on make"
-    exit 2
+    exit 3
 fi
 
 echo
 echo "*** make install"
 make install
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
     echo "ERROR on make install"
-    exit 3
+    exit 4
 fi
 
 echo
