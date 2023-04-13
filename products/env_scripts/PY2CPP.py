8c78ee737797637337bd902e27030f2e3b0ef68c
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2023-04-13 09:11:13 +0200

    openturns/persalys scripts on Windows - ongoing

--- products/env_scripts/PY2CPP.py
+++ products/env_scripts/PY2CPP.py
@@ -6,7 +6,10 @@ import platform
 def set_env(env, product_dir, version):
     env.set('PY2CPP_ROOT_DIR', product_dir)
     env.set('Py2cpp_DIR', os.path.join(product_dir, 'lib', 'cmake', 'py2cpp'))
-    env.prepend('LD_LIBRARY_PATH', os.path.join(product_dir, 'lib'))
+    if not platform.system() == "Windows" :
+        env.prepend('LD_LIBRARY_PATH', os.path.join(product_dir, 'lib'))
+    else:
+        env.prepend('PATH', os.path.join(product_dir, 'bin'))
 
 def set_nativ_env(env):
     pass
