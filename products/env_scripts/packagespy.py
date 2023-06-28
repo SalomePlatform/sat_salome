f9a154aff5755fad2ede377656ad7546a3843509
mbs <martin.bernhard@opencascade.com>
2023-06-28 17:37:54 +0100

    avoid 'OSError: [Errno 6] No such device or address' when calling set_env() method inside a Docker container for automatic builds

--- products/env_scripts/packagespy.py
+++ products/env_scripts/packagespy.py
@@ -4,9 +4,17 @@
 import os
 import platform
 import os
+import getpass
+
+def get_login():
+    try:
+        return os.getlogin()
+    except:
+        return getpass.getuser()
+
 def set_env(env, prereq_dir, version):
     env.set("PACKAGESPY_ROOT_DIR",prereq_dir)
-    env.set("WORKDIR4LOG",os.path.join('tmp', os.getlogin()))
+    env.set("WORKDIR4LOG",os.path.join('tmp', get_login()))
     env.prepend('PYTHONPATH',os.path.join(prereq_dir, 'packagespy'))
     env.prepend('PATH',os.path.join(prereq_dir,'packagespy','bin'))
 
