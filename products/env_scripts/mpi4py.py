e285141b7112e44fb975803e231f1f72338918dd
Guytri KASTANE <guytri.kastane@csgroup.eu>
2024-03-27 11:24:53 +0100

    spns #30166: fixes aimed to support UB18.04 for universal binary

--- products/env_scripts/mpi4py.py
+++ products/env_scripts/mpi4py.py
@@ -9,6 +9,7 @@ def set_env(env, prereq_dir, version):
   else :
     versionPython = env.get('PYTHON_VERSION')
     env.set("MPI4PY_ROOT_DIR",os.path.join(prereq_dir, 'lib', 'python' + versionPython, 'site-packages'))
+    env.prepend("PYTHONPATH",os.path.join(prereq_dir, 'lib', 'python' + versionPython, 'site-packages'))
 
 def set_nativ_env(env):
     pass
