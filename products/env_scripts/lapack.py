2c79d192fbf05cad2871fe5b6ac19e556e794976
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2024-10-04 14:45:32 +0200

    bos #42837: handle Fedora 32 and 34, embed lapack for these two platforms

--- products/env_scripts/lapack.py
+++ products/env_scripts/lapack.py
@@ -17,19 +17,23 @@ def set_env(env, prereq_dir, version):
     env.set('BLAS', os.path.join(prereq_dir, 'lib'))
     env.set('LAPACK', os.path.join(prereq_dir, 'lib'))
     env.set('ATLAS', os.path.join(prereq_dir, 'lib'))
+    env.set('LAPACK_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'lapack-3.8.0'))
     env.set('LAPACKE_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'lapacke-3.8.0'))
     env.set('LAPACKE_INCDIR', os.path.join(prereq_dir,'include'))
     env.set('LAPACKE_LIBDIR', os.path.join(prereq_dir,'lib'))
+    env.set('CBLAS_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'cblas-3.8.0'))
     env.set('CBLAS_ROOT_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'cblas-3.8.0'))
   else:
     env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
     env.set('BLAS', os.path.join(prereq_dir, 'lib'))
     env.set('LAPACK', os.path.join(prereq_dir, 'lib'))
     env.set('ATLAS', os.path.join(prereq_dir, 'lib'))
+    env.set('LAPACK_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'lapack-3.8.0'))
     env.set('LAPACKE_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'lapacke-3.8.0'))
     env.set('LAPACKE_INCDIR', os.path.join(prereq_dir,'include'))
     env.set('LAPACKE_LIBDIR', os.path.join(prereq_dir,'lib'))
     env.set('CBLAS_ROOT_DIR', os.path.join(prereq_dir,'lib', 'cmake', 'cblas-3.8.0'))
+
 def set_nativ_env(env):
     env.set('LAPACKHOME', '/usr')
     env.set('LAPACK_ROOT_DIR', '/')
