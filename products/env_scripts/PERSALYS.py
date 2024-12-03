e7a3985081cb8a450405102e5107f5414d34e042
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2024-12-03 08:56:33 +0100

    bos #43653 [COTECH] Integration of Persalys 17.0.1:  use PERSALYS_HTML_PATH environment variable for documentation

--- products/env_scripts/PERSALYS.py
+++ products/env_scripts/PERSALYS.py
@@ -16,8 +16,7 @@ def set_env(env, prereq_dir, version):
         env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
         pyver = 'python' + env.get('PYTHON_VERSION')
         env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'lib', pyver, 'site-packages'))
-    env.set('OTGUI_HTML_DOCUMENTATION_PATH',os.path.join(prereq_dir,'share','otgui','doc','html/'))
-    env.set('PERSALYS_HTML_DOCUMENTATION_PATH', os.path.join(prereq_dir,'share','doc','persalys','html/'))
+    env.set('PERSALYS_HTML_PATH', os.path.join(prereq_dir,'share','doc','persalys','html'))
 
 def set_nativ_env(env):
     pass
