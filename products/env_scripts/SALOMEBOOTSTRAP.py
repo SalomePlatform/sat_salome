e86b3c63d63eed473f39e7eb2fe3b0d20e88f3c9
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2025-01-06 14:55:33 +0100

    Expand SALOME_BOOTSTRAP environment

--- products/env_scripts/SALOMEBOOTSTRAP.py
+++ products/env_scripts/SALOMEBOOTSTRAP.py
@@ -6,7 +6,13 @@ import platform
 
 def set_env(env, prereq_dir, version):
   env.set('SALOMEBOOTSTRAP_ROOT_DIR', prereq_dir)
+  env.set('SALOME_APPLICATION_DIR', prereq_dir)
   env.prepend('PYTHONPATH', os.path.join(prereq_dir, '__SALOME_BOOTSTRAP__'))
+  env.prepend('PYTHONPATH', os.path.join(prereq_dir, '__RUN_SALOME__', 'lib', 'salome'))
+  if platform.system() == "Windows" :
+    env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, '__RUN_SALOME__', 'lib', 'salome'))
+  else:
+    env.prepend('PATH', os.path.join(prereq_dir, '__RUN_SALOME__', 'lib', 'salome'))
 
 def set_nativ_env(env):
   pass
