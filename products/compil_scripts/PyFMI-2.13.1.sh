0fe8184d741785be7bafbdf5a2110f1b45592ba7
Frederic PONS <frederic.pons@opencascade.com>
2025-01-29 11:47:08 +0100

    bos #43712: support Fedora 40

--- products/compil_scripts/PyFMI-2.13.1.sh
+++ products/compil_scripts/PyFMI-2.13.1.sh
@@ -4,6 +4,8 @@ echo "##########################################################################
 echo "PyFMI " $VERSION
 echo "##########################################################################"
 
+LINUX_DISTRIBUTION="$DIST_NAME$DIST_VERSION"
+
 rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
@@ -13,11 +15,23 @@ rm -f $BUILD_DIR/src/pyfmi/*.c
 export PATH=$(pwd)/bin:$PATH
 export PYTHONPATH=$(pwd):$PYTHONPATH
 
-echo 
-echo "*** ${PYTHONBIN} -m pip install . --prefix=${PRODUCT_INSTALL}"
-if ! ${PYTHONBIN} -m pip install . --prefix=${PRODUCT_INSTALL} -vvv; then
-    echo "Error on pip install"
-fi
+case $LINUX_DISTRIBUTION in
+    FD40)
+        echo
+        echo "*** ${PYTHONBIN} -m pip install PyFMI-2.13.0-cp312-cp312-linux_x86_64.whl --prefix=${PRODUCT_INSTALL}"
+        if ! ${PYTHONBIN} -m pip install PyFMI-2.13.0-cp312-cp312-linux_x86_64.whl --prefix=${PRODUCT_INSTALL}; then
+           echo "Error on pip install"
+        fi 
+        ;;
+    *)
+        echo
+        echo "*** ${PYTHONBIN} -m pip install . --prefix=${PRODUCT_INSTALL}"
+        if ! ${PYTHONBIN} -m pip install . --no-deps --no-build-isolation --prefix=${PRODUCT_INSTALL} -vvv; then
+            echo "Error on pip install"
+            exit 1
+        fi
+        ;;
+esac
 
 if [ -d ${PRODUCT_INSTALL}/local ];then
     mv ${PRODUCT_INSTALL}/local/* ${PRODUCT_INSTALL}
@@ -28,5 +42,9 @@ if [ -d ${PRODUCT_INSTALL}/lib/python${PYTHON_VERSION}/dist-packages ]; then
     mv ${PRODUCT_INSTALL}/lib/python${PYTHON_VERSION}/dist-packages ${PRODUCT_INSTALL}/lib/python${PYTHON_VERSION}/site-packages
 fi
 
+if [ -d ${PRODUCT_INSTALL}/lib64 ]; then
+    mv ${PRODUCT_INSTALL}/lib64 ${PRODUCT_INSTALL}/lib
+fi
 echo
+
 echo "########## END"
--- products/compil_scripts/assimulo.sh
+++ products/compil_scripts/assimulo.sh
@@ -4,15 +4,28 @@ echo "##########################################################################
 echo Assimulo $VERSION
 echo "##########################################################################"
 
+LINUX_DISTRIBUTION="$DIST_NAME$DIST_VERSION"
+
 cd $BUILD_DIR
 cp -R $SOURCE_DIR/* .
 
-echo 
-echo "*** ${PYTHONBIN} -m pip install . --prefix=${PRODUCT_INSTALL}"
-if ! ${PYTHONBIN} -m pip install . --prefix=${PRODUCT_INSTALL} -vvv; then
-    echo "Error on pip install"
-    exit 1
-fi
+case $LINUX_DISTRIBUTION in
+    FD40)
+        echo
+        echo "*** ${PYTHONBIN} -m pip install Assimulo-3.5.2-cp312-cp312-linux_x86_64.whl --prefix=${PRODUCT_INSTALL}"
+        if ! ${PYTHONBIN} -m pip install Assimulo-3.5.2-cp312-cp312-linux_x86_64.whl --prefix=${PRODUCT_INSTALL}; then
+           echo "Error on pip install"
+        fi 
+        ;;
+    *)
+        echo
+        echo "*** ${PYTHONBIN} -m pip install . --prefix=${PRODUCT_INSTALL}"
+        if ! ${PYTHONBIN} -m pip install . --no-deps --no-build-isolation --prefix=${PRODUCT_INSTALL} -vvv; then
+            echo "Error on pip install"
+            exit 1
+        fi
+        ;;
+esac
 
 if [ -d ${PRODUCT_INSTALL}/local ];then
     mv ${PRODUCT_INSTALL}/local/* ${PRODUCT_INSTALL}
