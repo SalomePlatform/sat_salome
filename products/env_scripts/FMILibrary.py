300bcf9ff3891830056634483397616f6318f63b
Guytri KASTANE <guytri.kastane@cea.fr>
2025-02-04 10:24:27 +0100

    bos #43921: update configuartions files for root-6.28.10 and uranie-4.8.0

--- products/env_scripts/FMILibrary.py
+++ products/env_scripts/FMILibrary.py
@@ -4,9 +4,12 @@ import platform
 def set_env(env, prereq_dir, version):
     env.set('FMILIBRARY_ROOT_DIR', prereq_dir)
     env.set('FMIL_HOME',prereq_dir)
+    env.set('FMILibrary_ROOT',prereq_dir)
     env.prepend('PATH', os.path.join(prereq_dir,'bin'))
     if not platform.system() == "Windows" :
         env.prepend('LD_LIBRARY_PATH',os.path.join(prereq_dir, 'lib'))
 
 def set_nativ_env(env):
     pass
+
+
