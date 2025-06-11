ee35812ade4649e383cfc2b6ba9652cabc49c5f9
Guytri KASTANE <guytri.kastane@cea.fr>
2025-06-11 10:30:20 +0200

    remove pytest installation when compiling C3PO

--- products/compil_scripts/C3PO-v2.3.sh
+++ products/compil_scripts/C3PO-v2.3.sh
@@ -21,19 +21,7 @@ if ! ${PYTHONBIN} -m pip install . --prefix="${PRODUCT_INSTALL}" --cache-dir="${
     exit 2
 fi
 
-if [ "${SAT_Python_IS_NATIVE}" == "1" ] && [ "$PYTEST_ROOT_DIR" != "" ]; then
-    PYTEST=$(which pytest)
-fi
-
-if [ "${SAT_Python_IS_NATIVE}" != "1" ] || [ -z "${PYTEST}" ]; then
-    echo
-    echo "*** install with $PYTHONBIN -m pip install ${BUILD_DIR}/ext/pytest-8.1.1.tar.gz --cache-dir=${BUILD_DIR}/cache/pip"
-    if ! ${PYTHONBIN} -m pip install "${BUILD_DIR}/ext/pytest-8.1.1.tar.gz" --prefix="${PRODUCT_INSTALL}" --cache-dir="${BUILD_DIR}/cache/pip"; then
-        echo "pip install pytest fails"
-        echo 3
-    fi
-    export PATH="${PRODUCT_INSTALL}/bin":$PATH
-fi
+PYTEST=$(which pytest)
 
 if [ -d ${PRODUCT_INSTALL}/local ];then
     mv ${PRODUCT_INSTALL}/local/* ${PRODUCT_INSTALL}
@@ -44,16 +32,21 @@ if [ -d ${PRODUCT_INSTALL}/lib/python${PYTHON_VERSION}/dist-packages ]; then
     mv ${PRODUCT_INSTALL}/lib/python${PYTHON_VERSION}/dist-packages ${PRODUCT_INSTALL}/lib/python${PYTHON_VERSION}/site-packages
 fi
 
-echo
-echo "*** running C3PO non-regression tests"
-export LD_LIBRARY_PATH="${MEDCOUPLING_ROOT_DIR}/lib:${LD_LIBRARY_PATH}"
-export PYTHONPATH="${MEDCOUPLING_ROOT_DIR}/${PYTHON_LIBDIR}:${PYTHONPATH}"
-export PYTHONPATH="${MEDCOUPLING_ROOT_DIR}/lib:${PYTHONPATH}"
-export PYTHONPATH="${PRODUCT_INSTALL}/lib/python${PYTHON_VERSION}/site-packages:${PYTHONPATH}"
+if [ -n $PYTEST ]; then
+    echo
+    echo "*** running C3PO non-regression tests"
+    export LD_LIBRARY_PATH="${MEDCOUPLING_ROOT_DIR}/lib:${LD_LIBRARY_PATH}"
+    export PYTHONPATH="${MEDCOUPLING_ROOT_DIR}/${PYTHON_LIBDIR}:${PYTHONPATH}"
+    export PYTHONPATH="${MEDCOUPLING_ROOT_DIR}/lib:${PYTHONPATH}"
+    export PYTHONPATH="${PRODUCT_INSTALL}/lib/python${PYTHON_VERSION}/site-packages:${PYTHONPATH}"
 
-if ! "${BUILD_DIR}"/run_tests.sh; then
-    echo "C3PO non-regression tests fails"
-    exit 3
+    if ! "${BUILD_DIR}"/run_tests.sh; then
+        echo "C3PO non-regression tests fails"
+        exit 3
+    fi
+else
+    echo
+    echo "WARNING: pytest not found. C3PO non-regression tests skipped."
 fi
 
 echo
