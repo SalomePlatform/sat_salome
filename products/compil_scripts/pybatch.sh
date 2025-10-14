478f52eda84a42f4dc778a387e149f2d56edddca
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2025-10-14 15:38:50 +0200

    bos #47355: wip

--- products/compil_scripts/pybatch.sh
+++ products/compil_scripts/pybatch.sh
@@ -12,16 +12,11 @@ mkdir -p ${BUILD_DIR}/cache/pip
 cd "${BUILD_DIR}"
 cp -r "${SOURCE_DIR}"/* .
 
-if [ "${SAT_Python_IS_NATIVE}" != "1" ]; then
-    echo
-    echo "*** install with $PYTHONBIN -m pip install . --cache-dir=${BUILD_DIR}/cache/pip --no-deps "
-    ${PYTHONBIN} -m pip install . --prefix=$PRODUCT_INSTALL --cache-dir=$BUILD_DIR/cache/pip  --no-deps -vvvv
-    if [ $? -ne 0 ]; then
-        echo "pip install pybatch fails"
-        exit 1
-    fi
+${PYTHONBIN} -m pip install . --prefix=$PRODUCT_INSTALL --cache-dir=$BUILD_DIR/cache/pip  --no-deps -vvvv
+if [ $? -ne 0 ]; then
+    echo "pip install pybatch fails"
+    exit 1
 fi
-
 if [ -d ${PRODUCT_INSTALL}/local ];then
     mv ${PRODUCT_INSTALL}/local/* ${PRODUCT_INSTALL}
     rm -rf ${PRODUCT_INSTALL}/local
