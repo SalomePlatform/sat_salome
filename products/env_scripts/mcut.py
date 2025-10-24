a51966599b174078039a7bc1165ce2e70bd13bdc
Christophe Bourcier <christophe.bourcier@cea.fr>
2025-10-24 11:49:30 +0200

    [spns #43020] Add LD_LIBRARY_PATH for mcut, otherwise it does not work in meshboolean plugin

--- products/env_scripts/mcut.py
+++ products/env_scripts/mcut.py
@@ -8,6 +8,7 @@ def set_env(env, prereq_dir, version):
   env.set('MCUT_ROOT_DIR', prereq_dir)
   env.set('MCUT_VERSION', version)
   env.prepend('PATH', os.path.join(prereq_dir, 'bin'))
+  env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
 
 def set_nativ_env(env):
   pass
