982bcd95118a99a7e6c3bb8d69a522c39e0d103d
Guytri KASTANE <guytri.kastane@cea.fr>
2025-11-05 13:57:51 +0100

    [bos #47841] upgrade GMSH version to 4.14.1

--- products/env_scripts/gmsh.py
+++ products/env_scripts/gmsh.py
@@ -9,6 +9,10 @@ def set_env(env, prereq_dir, version):
   env.set('GMSH_ROOT_DIR', prereq_dir)
   env.append('PATH',os.path.join(prereq_dir,'bin'))
   if platform.system() == "Windows" :
+    env.set('GMSH_DIR', os.path.join(prereq_dir, 'share', 'gmsh'))
+    env.prepend('GMSH_INCLUDE_DIRS', os.path.join(prereq_dir, 'include'))
+    env.prepend('GMSH_INCLUDE_DIRS', os.path.join(prereq_dir, 'include', 'gmsh'))
+    env.set('GMSH_LIBRARIES', os.path.join(prereq_dir, 'bin', 'gmsh.dll'))
     env.prepend('PYTHONPATH', os.path.join(prereq_dir, 'bin'))
   else:
     env.prepend('LD_LIBRARY_PATH', os.path.join(prereq_dir, 'lib'))
