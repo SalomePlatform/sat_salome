0289d3eb9b254dc2aa9a5e0d313cbd902a368d12
Guytri KASTANE <guytri.kastane@cea.fr>
2026-05-04 13:26:49 +0200

    sip-5.5.0: cleanup obsolete conditional check

--- products/compil_scripts/sip-5.5.0.sh
+++ products/compil_scripts/sip-5.5.0.sh
@@ -96,42 +96,40 @@ case $LINUX_DISTRIBUTION in
         ;;
 esac
 
-if [ $LINUX_DISTRIBUTION != "DB13FIXME" ]; then
-    cd $BUILD_DIR
-    cp -R $SOURCE_DIR/PyQt5_sip-$PYQT5_SIP_VERSION $BUILD_DIR/PyQt5_sip-$PYQT5_SIP_VERSION
-    cd $BUILD_DIR/PyQt5_sip-$PYQT5_SIP_VERSION
+cd $BUILD_DIR
+cp -R $SOURCE_DIR/PyQt5_sip-$PYQT5_SIP_VERSION $BUILD_DIR/PyQt5_sip-$PYQT5_SIP_VERSION
+cd $BUILD_DIR/PyQt5_sip-$PYQT5_SIP_VERSION
 
-    echo
-    echo "*** build with $PYTHONBIN"
-    if [ "$USE_SETUP" == "true" ]; then
-        $PYTHONBIN setup.py build
-        if [ $? -ne 0 ]
-        then
-            echo "ERROR on build"
-            exit 2
-        fi
-
-        echo
-        echo "*** install with $PYTHONBIN"
-        $PYTHONBIN setup.py install --prefix=$PRODUCT_INSTALL
-        if [ $? -ne 0 ]
-        then
-            echo "ERROR on install"
-            exit 3
-        fi
-    else
-        echo
-        echo "*** install with $PYTHONBIN"
-        $PYTHONBIN -m pip install --cache-dir=$BUILD_DIR/cache/pip  .  --no-deps --prefix=$PRODUCT_INSTALL
-        if [ $? -ne 0 ]
-        then
-            echo "ERROR on install"
-            exit 3
-        fi
+echo
+echo "*** build with $PYTHONBIN"
+if [ "$USE_SETUP" == "true" ]; then
+    $PYTHONBIN setup.py build
+    if [ $? -ne 0 ]
+    then
+        echo "ERROR on build"
+        exit 2
     fi
 
-    fix_lib_path
+    echo
+    echo "*** install with $PYTHONBIN"
+    $PYTHONBIN setup.py install --prefix=$PRODUCT_INSTALL
+    if [ $? -ne 0 ]
+    then
+        echo "ERROR on install"
+        exit 3
+    fi
+else
+    echo
+    echo "*** install with $PYTHONBIN"
+    $PYTHONBIN -m pip install --cache-dir=$BUILD_DIR/cache/pip  .  --no-deps --prefix=$PRODUCT_INSTALL
+    if [ $? -ne 0 ]
+    then
+        echo "ERROR on install"
+        exit 3
+    fi
 fi
+
+fix_lib_path
 
 mkdir $PRODUCT_INSTALL/include
 cp *.h $PRODUCT_INSTALL/include
