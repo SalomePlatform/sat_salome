2d72555b694eb8ee9f139f9aefd5a96b5b98b50f
Nabil Ghodbane <nabil.ghodbane@cea.fr>
2026-06-08 14:17:41 +0200

    [bos #55014] yacgen_module is missing on Debian 13

--- products/compil_scripts/YACSGEN.sh
+++ products/compil_scripts/YACSGEN.sh
@@ -17,7 +17,7 @@ echo
 echo "*** build and install with $PYTHONBIN"
 echo "*** $LINUX_DISTRIBUTION *"
 case $LINUX_DISTRIBUTION in
-    DB12|UB24*)
+    DB13|DB12|UB24*)
         $PYTHONBIN -m pip install . --prefix=$PRODUCT_INSTALL
         ;;
     *)
