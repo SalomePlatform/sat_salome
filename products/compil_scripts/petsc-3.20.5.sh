413da15ad67c4b26a08b6f650e2ac93957d3550a
Leon Kos <leon.kos@lecad.fs.uni-lj.si>
2026-07-08 11:30:11 +0200

    PETSC should not mix -opt and -debug architecture due to SLEPC flags collission

--- products/compil_scripts/petsc-3.20.5.sh
+++ products/compil_scripts/petsc-3.20.5.sh
@@ -76,6 +76,7 @@ if [ -n "${SAT_DEBUG}" ]; then
    CONFIGURE_FLAGS+=" --with-debugging=1" # default
 else
    CONFIGURE_FLAGS+=" --with-debugging=0"
+   CONFIGURE_FLAGS+=" --PETSC_ARCH=installed-arch-linux2-c-opt"
 fi
 
 CONFIGURE_FLAGS+=" --with-petsc4py=yes"
@@ -127,7 +128,6 @@ else
    CONFIGURE_FLAGS+=" --with-mpi=0"
 fi
 
-CONFIGURE_FLAGS+=" --PETSC_ARCH=installed-arch-linux2-c-opt"
 CONFIGURE_FLAGS+=" --PETSC_DIR=${BUILD_DIR}"
 
 echo "*** configure --prefix=${PRODUCT_INSTALL} ${CONFIGURE_FLAGS}"
