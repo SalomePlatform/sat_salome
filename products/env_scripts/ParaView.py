abd429d81f246304823575bab639bc18baad8514
Guytri KASTANE <guytri.kastane@cea.fr>
2026-08-25 10:54:32 +0200

    [spns #56763] Update ParaView environment script

--- products/env_scripts/ParaView.py
+++ products/env_scripts/ParaView.py
@@ -1,90 +1,95 @@
 #!/usr/bin/env python
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 
 import os.path
 import platform
 
+
 def set_env(env, prereq_dir, version):
     # keep only the first two version numbers
-    ver = '.'.join(version.replace('-', '.').split('.')[:2])
+    ver = ".".join(version.replace("-", ".").split(".")[:2])
 
     # BUG WITH 5.0_beta
-    if ver == '5.0_beta':
-        ver = '5.0'
+    if ver == "5.0_beta":
+        ver = "5.0"
 
-    if ver == '5.8.0rc2' :
-        ver = '5.8'
+    if ver == "5.8.0rc2":
+        ver = "5.8"
 
-    env.set('PVHOME', prereq_dir)
-    env.set('VTKHOME', prereq_dir)  
-    env.set('PVVERSION', ver)
-    
-    env.set('PARAVIEW_ROOT_DIR', prereq_dir)
-    env.set('PARAVIEW_VERSION', ver)
+    env.set("PVHOME", prereq_dir)
+    env.set("VTKHOME", prereq_dir)
+    env.set("PVVERSION", ver)
+
+    env.set("PARAVIEW_ROOT_DIR", prereq_dir)
+    env.set("PARAVIEW_VERSION", ver)
 
     set_paraview_env(env, ver)
     set_vtk_env(env, ver)
 
+
 def set_nativ_env(env):
     if os.getenv("PVHOME") is None:
         raise Exception("PVHOME is not set")
-    
+
     if os.getenv("PVVERSION") is None:
         raise Exception("PVVERSION is not set")
 
     version = env.get("PVVERSION")
     set_paraview_env(env, version)
 
+
 def set_paraview_env(env, version):
-    root = env.get('PVHOME')
-    env.prepend('PATH', os.path.join(root, 'bin'))
-    if platform.system() == "Windows" :
+    root = env.get("PVHOME")
+    env.prepend("PATH", os.path.join(root, "bin"))
+    if platform.system() == "Windows":
         # see spns #48124 or bos #47676 - since we use PARAVIEW_VERSIONED_INSTALL=OFF
-        if version >= '6.0':
-            paralib = os.path.join(root, 'bin', 'paraview')
+        if version >= "6.0":
+            paralib = os.path.join(root, "bin", "paraview")
         else:
-            paralib = os.path.join(root, 'bin', 'paraview-%s' % version)
-        env.prepend('PATH', paralib)
-        env.set('PARAVIEW_PLUGINS_DIR',os.path.join(paralib, 'plugins'))
-        env.prepend('PV_PLUGIN_PATH', os.path.join(paralib, 'plugins'))
+            paralib = os.path.join(root, "bin", "paraview-%s" % version)
+        env.prepend("PATH", paralib)
+        env.set("PARAVIEW_PLUGINS_DIR", os.path.join(paralib, "plugins"))
+        env.prepend("PV_PLUGIN_PATH", os.path.join(paralib, "plugins"))
         # bos #45920
-        if version >= '5.13' :
-            site_packages = os.path.join(root, 'bin', 'Lib', 'site-packages')
-            env.prepend('PYTHONPATH', os.path.join(site_packages, 'vtk'))
-            env.prepend('PYTHONPATH', os.path.join(site_packages))
+        if version >= "5.13":
+            site_packages = os.path.join(root, "bin", "Lib", "site-packages")
+            env.prepend("PYTHONPATH", os.path.join(site_packages, "vtk"))
+            env.prepend("PYTHONPATH", os.path.join(site_packages))
         else:
-            site_packages = os.path.join(root, 'bin', 'Lib', 'site-packages')
-            env.prepend('PYTHONPATH', os.path.join(site_packages, 'paraview'))
-            env.prepend('PYTHONPATH', os.path.join(site_packages, 'vtk'))
-            env.prepend('PYTHONPATH', os.path.join(site_packages))
+            site_packages = os.path.join(root, "bin", "Lib", "site-packages")
+            env.prepend("PYTHONPATH", os.path.join(site_packages, "paraview"))
+            env.prepend("PYTHONPATH", os.path.join(site_packages, "vtk"))
+            env.prepend("PYTHONPATH", os.path.join(site_packages))
     else:
         # see spns #48124 or bos #47676 - since we use PARAVIEW_VERSIONED_INSTALL=OFF
-        if version >= '6.0':
-            paralib = os.path.join(root, 'lib', 'paraview')
+        if version >= "6.0":
+            paralib = os.path.join(root, "lib", "paraview")
         else:
-            paralib = os.path.join(root, 'lib', 'paraview-%s' % version)
+            paralib = os.path.join(root, "lib", "paraview-%s" % version)
 
-        env.set('PARAVIEW_PLUGINS_DIR',os.path.join(paralib, 'plugins'))
-        env.prepend('PV_PLUGIN_PATH', paralib)
+        env.set("PARAVIEW_PLUGINS_DIR", os.path.join(paralib, "plugins"))
+        env.prepend("PV_PLUGIN_PATH", paralib)
         # bos #26828
-        env.prepend('PV_PLUGIN_PATH', os.path.join(paralib, 'plugins'))
-        env.prepend('PYTHONPATH', os.path.join(paralib, 'site-packages'))
-        env.prepend('PYTHONPATH', os.path.join(paralib, 'site-packages', 'vtk'))
-        env.prepend('PATH', os.path.join(root, 'include', 'paraview'))
-        env.prepend('LD_LIBRARY_PATH', paralib)
-        env.prepend('PYTHONPATH', paralib)
+        env.prepend("PV_PLUGIN_PATH", os.path.join(paralib, "plugins"))
+        env.prepend("PYTHONPATH", os.path.join(paralib, "site-packages"))
+        env.prepend("PYTHONPATH", os.path.join(paralib, "site-packages", "vtk"))
+        env.prepend("PATH", os.path.join(root, "include", "paraview"))
+        env.prepend("LD_LIBRARY_PATH", paralib)
+        env.prepend("PYTHONPATH", paralib)
+
 
 def set_vtk_env(env, version):
-    root = env.get('VTKHOME')
-    pyver = 'python' + env.get('PYTHON_VERSION')
-    env.set('VTK_ROOT_DIR', root)
-    env.set('VTK_DIR', os.path.join(root, 'lib', 'cmake', 'paraview-' + version))
-    
-    if not platform.system() == "Windows" :
-        #http://computer-programming-forum.com/57-tcl/1dfddc136afccb94.htm
-        #Tcl treats the contents of that variable as a list. Be happy, for you can now use drive letters on windows.
-        env.prepend('LD_LIBRARY_PATH', os.path.join(root, 'lib'))
-        env.prepend('PYTHONPATH', os.path.join(root, 'lib', pyver, 'site-packages'))
-    if version == '3.98':
-        cmake_dir = os.path.join(root, 'lib', 'cmake', 'paraview-' + version)
-        env.set('VTK_DIR', cmake_dir)
+    root = env.get("VTKHOME")
+    pyver = "python" + env.get("PYTHON_VERSION")
+    env.set("VTK_ROOT_DIR", root)
+    if version >= "6.0":
+        cmake_dir = os.path.join(root, "lib", "cmake", "paraview")
+    else:
+        cmake_dir = os.path.join(root, "lib", "cmake", "paraview-%s" % version)
+    env.set("VTK_DIR", cmake_dir)
+
+    if not platform.system() == "Windows":
+        # http://computer-programming-forum.com/57-tcl/1dfddc136afccb94.htm
+        # Tcl treats the contents of that variable as a list. Be happy, for you can now use drive letters on windows.
+        env.prepend("LD_LIBRARY_PATH", os.path.join(root, "lib"))
+        env.prepend("PYTHONPATH", os.path.join(root, "lib", pyver, "site-packages"))
